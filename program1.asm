b1c5ed19e17190b85ea07bd53119e5cc1cb249cd
Kestrel86 <valdezandrew20@gmail.com>
2023-03-15 13:49:44 -0700

    Setup input for program

--- program1.asm
+++ program1.asm
@@ -4,7 +4,26 @@
 
 .data
 int: .word #int: .word (number here)
+msg: .asciiz "Enter a number: \n"
 
 .text
 main: 
+	#print msg
+	li $v0, 4
+	la $a0, msg
+	syscall
 	
+	li $v0, 5
+	syscall 
+	move $s1, $v0
+	
+	li $v1, 5
+      	syscall
+      	move $s2, $v1
+      	
+      	la $a0, 32($s1)
+
+
+      	
+      	#exit program
+	#li $v0, 10
