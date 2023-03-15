b329c0a40ffe1b9d9c2c724d9bc498f44ae6a660
Kestrel86 <valdezandrew20@gmail.com>
2023-03-14 23:39:18 -0700

    first commit

---
+++ program1.asm
@@ -0,0 +1,10 @@
+#Name: CS2640 Program 1
+#Date: March 26, 2023
+#Objective: Familiarize ourselves with differnt MIPS instructions, syscall services, MARS environment, and overall practice with Assembly programming
+
+.data
+int: .word #int: .word (number here)
+
+.text
+main: 
+	
