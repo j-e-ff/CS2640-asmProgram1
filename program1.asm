547c47063dd321d050f196531c987eecd732a726
Kestrel86 <valdezandrew20@gmail.com>
2023-03-15 15:07:13 -0700

    finished input/output message

--- program1.asm
+++ program1.asm
@@ -5,6 +5,8 @@
 .data
 int: .word #int: .word (number here)
 msg: .asciiz "Enter a number: \n"
+newLine: .asciiz "\n"
+outputMsg: .asciiz "This is the output: \n"
 
 .text
 main: 
@@ -13,17 +15,32 @@ main:
 	la $a0, msg
 	syscall
 	
+	#collect input
 	li $v0, 5
 	syscall 
 	move $s1, $v0
 	
-	li $v1, 5
+	li $v0, 5
       	syscall
-      	move $s2, $v1
+      	move $s2, $v0
       	
-      	la $a0, 32($s1)
-
-
+      	#output user input
+      	li $v0, 4
+	la $a0, outputMsg
+	syscall
+	
+      	move $a0, $s1
+      	li $v0, 1
+	syscall
+	
+	li $v0, 4
+	la $a0, newLine
+	syscall
+	
+	move $a0, $s2
+	li $v0, 1
+	syscall
       	
       	#exit program
-	#li $v0, 10
+	li $v0, 10
+	syscall
